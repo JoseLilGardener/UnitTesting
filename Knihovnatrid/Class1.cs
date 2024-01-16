bc8cd4fb9fdab9c94d56658e5bc0b2757cd6441f
zahradniceki.06 <zahradniceki.06@I102-34>
2024-01-16 08:23:59 +0100

    Přidejte soubory projektu.

---
+++ Knihovnatrid/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Knihovnatrid
+{
+    public class Class1
+    {
+        public override string ToString()
+        {
+            return "Ahoj";
+        }
+
+        public string RPR()
+        {
+            return "RPR";
+        }
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodPos()
+        {
+            Knihovnatrid.Class1 trida = new Knihovnatrid.Class1();
+            string ocekavane = "Ahoj";
+            Assert.AreEqual(ocekavane, trida.ToString());
+
+        }
+
+        [TestMethod]
+        public void TestMethodNeg()
+        {
+            Knihovnatrid.Class1 trida = new Knihovnatrid.Class1();
+            string ocekavane = "cAU";
+            Assert.AreEqual(ocekavane, trida.ToString());
+
+        }
+        [TestMethod]
+        public void TestMethodRPR()
+        {
+            Knihovnatrid.Class1 trida = new Knihovnatrid.Class1();
+            string ocekavane = "ncwibefowbfweoRPRocbqwoebfoqwe";
+            Assert.IsTrue(ocekavane.Contains(trida.RPR()));
+        }
+        [TestMethod]
+        public void TestMethodRPRNeg()
+        {
+            Knihovnatrid.Class1 trida = new Knihovnatrid.Class1();
+            string ocekavane = "ncwibefowbfweoRPocbqwoebfoqwe";
+            Assert.IsTrue(ocekavane.Contains(trida.RPR()));
+        }
+        [TestMethod]
+        public void TestMethodHeslo()
+        {
+            Knihovnatrid.Class1 trida = new Knihovnatrid.Class1();
+            string heslo = "12345678";
+            Assert.IsTrue(heslo.Length >= 8 && heslo.Length <= 20 && heslo != "");
+        }
+        [TestMethod]
+        public void TestMethodHesloNeg()
+        {
+            Knihovnatrid.Class1 trida = new Knihovnatrid.Class1();
+            string heslo = "";
+            Assert.IsTrue(heslo.Length >= 8 && heslo.Length <= 20 && heslo != "");
+        }
+    }
+}
